ff986b8d82ab89329944214ddc0500d3ea1a4903
bgmaduragoda <bgmaduragoda@gmail.com>
2023-12-31 15:19:17 +0000

    set telwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
+      <div>
+        <p className='text-9xl'>
+          Get d by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
